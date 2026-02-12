52e21fe87ac6dab2bbca3f3de19205e5eb540468
Alexander Grahn <agrahn@users.noreply.github.com>
2026-02-12 20:35:16 +0100

    cached pgp auth key passphrase with device lock protection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
   defaultConfig {
     minSdk = 26
-    applicationId = "app.passwordstore.agrahn"
+    applicationId = "app.passwordstore2.agrahn"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
