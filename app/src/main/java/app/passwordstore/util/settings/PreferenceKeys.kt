0b9d6b3d1886d8bb57cbc845a9a2c01baef15131
Alexander Grahn <agrahn@users.noreply.github.com>
2026-09-03 13:33:01 +0200

    autofill: persistent settings for switches on search pane (#1062)

--- app/src/main/java/app/passwordstore/util/settings/PreferenceKeys.kt
+++ app/src/main/java/app/passwordstore/util/settings/PreferenceKeys.kt
@@ -59,6 +59,8 @@ object PreferenceKeys {
   const val OREO_AUTOFILL_DEFAULT_USERNAME = "oreo_autofill_default_username"
   const val DIRECTORY_STRUCTURE = "oreo_autofill_directory_structure"
   const val STRICT_DOMAIN_SEARCH = "oreo_autofill_strict_domain_search"
+  const val DOMAIN_QUICK_SELECT_ADD = "oreo_autofill_domain_quick_select_add"
+  const val DOMAIN_QUICK_SELECT_REMOVE = "oreo_autofill_domain_quick_select_remove"
   const val PREF_KEY_PWGEN_TYPE = "pref_key_pwgen_type"
   const val REPOSITORY_INITIALIZED = "repository_initialized"
   const val REPO_CHANGED = "repo_changed"
